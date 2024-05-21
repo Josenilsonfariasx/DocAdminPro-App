0bd1ad267c1763e411f0d81194cf72616e6dc216
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-21 10:23:49 -0300

    feat: adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import "./styles/index.scss"
+import { RoutesMain } from "./routes/routes"
+import "react-toastify/dist/ReactToastify.css"
+import { ToastContainer } from "react-toastify"
 function App() {
   return (
     <>
+      <RoutesMain />
+      <ToastContainer
+        position="top-center"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        />
     </>
   )
 }
---
+++ src/routes/routes.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import { UserProvider } from "../Providers/UserContex"
+import { Register }  from "../Pages/Register/Register"
+
+export const RoutesMain = () => {
+    return (
+        <UserProvider>
+            <Routes>
+                {/* <Route path="/" element={<SingIn />} /> */}
+                <Route path="/register" element={<Register /> } />
+                {/* <Route path="/home" element={<ProtectedRoutes />}>
+                    <Route index element={ <TechProvider><Dashboard /></TechProvider>} />
+                </Route> */}
+            </Routes>
+        </UserProvider>
+    )
+}

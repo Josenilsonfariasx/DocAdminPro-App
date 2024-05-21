7d3cdd9d0a8d933aef393dc96ac69da0884ebc9e
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-21 11:11:14 -0300

    feat: adding login page

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react"
+import { useUserContext } from "../../Providers/UserContex"
+import { useNavigate } from "react-router-dom"
+import { ValidationLogin } from "./ValidationLogin"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Header } from "../../components/Header/Header"
+import { Input } from "../../components/Input/Input"
+import { useForm } from "react-hook-form"
+import { toast } from "react-toastify"
+import style from "./style.module.scss"
+
+export const SingIn = () => {
+    const [loading, setLoading] = useState()
+    const { login } = useUserContext()
+    const navi = useNavigate()
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(ValidationLogin),
+    })
+
+    const submit = (dataForm) => {
+        try {
+            setLoading(true)
+            login(dataForm)
+        } catch (error) {
+            toast.warning(error.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <div className={style.div}>
+            <Header visible={false} />
+            <main className={style.main}>
+                <h1 className="title white">Login</h1>
+                <div>
+                    <form onSubmit={handleSubmit(submit)}>
+                        <div>
+                            <Input
+                                label="Email"
+                                type="text"
+                                placeholder="Digite seu email"
+                                {...register("email")}
+                                error={errors.email}
+                            />
+                            <Input
+                                label="senha"
+                                type="password"
+                                placeholder="Digite seu senha"
+                                {...register("password")}
+                                error={errors.password}
+                            />
+                            <button className="button">
+                                {!loading ? "Entrar" : "Tentando conectar"}
+                            </button>
+                        </div>
+                    </form>
+                    <span className="title headline grey">
+                        Ainda não possui uma conta?
+                    </span>
+                    <button
+                        className="button variant"
+                        onClick={() => navi("/register")}
+                    >
+                        Cadastre-se
+                    </button>
+                </div>
+            </main>
+        </div>
+    )
+}
